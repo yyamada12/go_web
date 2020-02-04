0d5145df5312f2565cf6e27ff2b0449a3b2a1d3f
yyamada12 <12yacropolisy@gmail.com>
2020-02-05 08:06:29 +0900

    add css

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("views/*.html")
+	router.Static("/assets", "./assets")
 
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.HTML(http.StatusOK, "index.html", gin.H{})
