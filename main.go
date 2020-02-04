be7997f6f3324b7880d5ca4ec3910dfd1cb7c8d4
yyamada12 <12yacropolisy@gmail.com>
2020-02-04 23:26:41 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("views/*.html")
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "index.html", gin.H{})
+	})
+
+	router.Run(":8080")
+
+}
